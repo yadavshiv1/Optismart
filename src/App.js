84d653c94809721eeb5fa4cdda1da9e02d016137
Apple <username@example.com>
2024-06-30 19:02:41 +0530

    added more pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import AboutUs from './AboutUs';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import AboutUs from './AboutUs';
 import ContactModel from './ContactModel';
 import MainPage from './MainPage';
 import OurProduct from './OurProduct';
-import Services from './Service';
 
 function App() {
-  return (  
-    <MainPage />
-      );
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/our-product" element={<OurProduct />} />
+          <Route path="/contact-us" element={<ContactModel />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,28 @@
-import React from 'react'
-import Logo from '../src/logo.webp'
+import React, { useState } from 'react';
+import Logo from '../src/logo.webp';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
-    <img src={Logo} alt="logo_img" className="logo" />
-    <div className="right-navbar">
-      <div>Home</div>
-      <div>About</div>
-      <div>Our Product</div>
-      <div>Contact us</div>
-    </div>
-  </nav>
-  )
+      <img src={Logo} alt="logo_img" className="logo" />
+      <div className="menu-toggle" onClick={toggleMenu}>
+        <div className="menu-icon"></div>
+        <div className="menu-icon"></div>
+        <div className="menu-icon"></div>
+      </div>
+      <div className={`right-navbar ${isOpen ? 'active' : ''}`}>
+        <div><Link to="/" className='nav-link' >Home</Link></div>
+        <div><Link to="/about" className='nav-link' onClick={toggleMenu}>About</Link></div>
+        <div><Link to="/our-product" className='nav-link' onClick={toggleMenu}>Our Product</Link></div>
+        <div><Link to="/contact-us" className='nav-link' onClick={toggleMenu}>Contact</Link></div>
+      </div>
+    </nav>
+  );
 }
